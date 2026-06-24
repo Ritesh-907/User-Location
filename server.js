941d227db7ba7ddc4f22de6b9d2c49718a541a97
Ritesh-907 <chaudharyritesh.7777@gmail.com>
2026-06-24 18:43:02 +0530

    last last final commit

--- server.js
+++ server.js
@@ -25,21 +25,31 @@ app.get("/", (req, res) => {
 app.get("/save", async (req, res) => {
   try {
     const userIp =
-      req.headers["x-forwarded-for"]?.split(",")[0] ||
-      req.socket.remoteAddress;
+      req.headers["x-forwarded-for"]?.split(",")[0] || req.socket.remoteAddress;
 
-    const response = await fetch(
-      `https://ipinfo.io/${userIp}/json`
-    );
+    const response = await fetch(`https://ipinfo.io/${userIp}/json`);
 
     const data = await response.json();
+    const saveResponse = await fetch(GOOGLE_SCRIPT_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
 
+    const googleBody = await saveResponse.text();
+
+    res.json({
+      success: true,
+      googleResponse: googleBody,
+      data,
+    });
     res.json({
       success: true,
       visitorIp: userIp,
       data,
     });
-
   } catch (error) {
     res.status(500).json({
       success: false,
